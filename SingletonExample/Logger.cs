d8c0743265fa05ac61129c2136977099e2741b72
naimolcer <naimolcer@gmail.com>
2024-03-01 16:40:06 +0300

    Add project files.

---
+++ SingletonExample/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    public class Logger
+    {
+        private static Logger _logger;
+
+        private Logger() { }
+
+        public static Logger CrateAsSingleton()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+
+            return _logger;
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine(@"Info - {message}");
+        }
+        public void Error(string message)
+        {
+            Console.WriteLine(@"Error - {message}");
+        }
+    }
+}
---
+++ SingletonExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Logger log1 = Logger.CrateAsSingleton();
+            Logger log2 = Logger.CrateAsSingleton();
+            Logger log3 = Logger.CrateAsSingleton();
+
+            log3.Info("bla bla");
+        }
+    }
+}
